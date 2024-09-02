788d5e727c36f7f1573ead56c5af1e51c8acabe3
Miguel Amores <migueamores@gmail.com>
2024-09-02 09:52:52 -0500

    fix results height

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,19 +1,11 @@
 import { getVotes } from '@/actions';
 import ChartVotes from '@/components/ChartVotes';
 
-const chartData = [
-  { month: 'January', mobile: 80 },
-  { month: 'February', mobile: 200 },
-  { month: 'March', mobile: 120 },
-  { month: 'April', mobile: 190 },
-  { month: 'May', mobile: 130 },
-];
-
 const result = async () => {
   const votes = await getVotes();
   console.log(': ------ ', votes);
   return (
-    <section className='p-10 h-screen'>
+    <section className='p-10 h-[90dvh]'>
       <ChartVotes votes={votes} />
     </section>
   );
